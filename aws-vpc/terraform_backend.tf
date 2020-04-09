af7de589ac617fda71830780a985270820118053
System Administrator <root@nav-macbook-pro-new.local>
2020-04-09 20:54:06 +0530

    Changed Backend Config & Modified GitIgnore

--- aws-vpc/terraform_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt        = true
-    bucket         = "devops-mumbai"
-    dynamodb_table = "tf-state-lock"
-    region         = "ap-south-1"
-    key            = ""
-  }
-}
