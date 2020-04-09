c1b87fabf704c8d7e8fbb183a036f5e35ad4df46
System Administrator <root@nav-macbook-pro-new.local>
2020-04-09 20:44:32 +0530

    Changed Backend Config

--- aws-vpc/terraform_backend.tf
+++ aws-vpc/terraform_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     encrypt        = true
-    bucket         = "devops-mumbai"
+    bucket         = ""
     dynamodb_table = "tf-state-lock"
     region         = "ap-south-1"
     key            = ""
